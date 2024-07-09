5d9dac040db26f1a4dfe0cdda54102ed7bf3a10e
claudiomarcosdf <claudio.marcos.df@gmail.com>
2024-07-09 12:03:48 -0300

    Cadastro de coroinha finalizado

--- src/components/ItemListaCoroinha/index.js
+++ src/components/ItemListaCoroinha/index.js
@@ -1,12 +1,36 @@
 import { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from 'react-native';
 
-export default function ItemListaCoroinha({ data }) {
+import { FontAwesome6 } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
+export default function ItemListaCoroinha({ data, deleteItem, editItem }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.textNome}>{data.nome}</Text>
-      <Text style={styles.textCelular}>{data.celular}</Text>
+      <View style={styles.personBox}>
+        <View style={styles.iconPersonBox}>
+          <FontAwesome6 name='person-praying' size={25} color='#2f3640' />
+        </View>
+        <View>
+          <TouchableWithoutFeedback onPress={() => editItem(data)}>
+            <Text style={styles.textNome}>{data.nome}</Text>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => editItem(data)}>
+            <Text style={styles.textCelular}>{data.celular}</Text>
+          </TouchableWithoutFeedback>
+        </View>
+      </View>
+      <View style={styles.deleteBox}>
+        <TouchableOpacity onPress={() => deleteItem(data.key)}>
+          <Ionicons name='trash-bin' size={20} color='#ff6b81' />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,19 +38,36 @@ export default function ItemListaCoroinha({ data }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     padding: 10,
-    marginTop: 10,
-    marginBottom: 0,
-    backgroundColor: '#57606f',
+    marginTop: 0,
+    marginBottom: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff'
   },
   textNome: {
-    color: '#fff',
+    color: '#2f3640',
     fontSize: 14,
     fontWeight: '700'
   },
   textCelular: {
-    color: '#fff',
+    color: '#2f3640',
     fontSize: 12
+  },
+  personBox: {
+    flexDirection: 'row'
+  },
+  iconPersonBox: {
+    padding: 2,
+    marginRight: 15,
+    borderRadius: 2,
+    backgroundColor: '#dfe4ea',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  deleteBox: {
+    alignItems: 'center',
+    justifyContent: 'center'
   }
-
 });
--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,23 +1,136 @@
-import { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, SafeAreaView, TouchableOpacity, FlatList, Keyboard } from 'react-native';
+import { useEffect, useState, useRef } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  FlatList,
+  Keyboard
+} from 'react-native';
 
+import firebase from '../../firebaseConfig';
 import ItemListaCoroinha from '../../components/ItemListaCoroinha';
 
-const coroinhas = [
-  { key: 1, nome: 'Therezinha', celular: '99236-4764' },
-  { key: 2, nome: 'João Pedro', celular: '99236-4764' },
-  { key: 3, nome: 'Othor', celular: '99236-4764' },
-  { key: 4, nome: 'Salete', celular: '99236-4764' },
-  { key: 5, nome: 'Mão Diná', celular: '99236-4764' },
-  { key: 6, nome: 'Mão Diná', celular: '99236-4764' },
-  { key: 7, nome: 'Mão Diná', celular: '99236-4764' },
-  { key: 8, nome: 'Mão Diná', celular: '99236-4764' },
-  { key: 9, nome: 'Mão Diná', celular: '99236-4764' },
-]
+// const coroinhas = [
+//   { key: 1, nome: 'Therezinha', celular: '99236-4764' },
+//   { key: 2, nome: 'João Pedro', celular: '99236-4764' },
+//   { key: 3, nome: 'Othor', celular: '99236-4764' },
+//   { key: 4, nome: 'Salete', celular: '99236-4764' },
+//   { key: 5, nome: 'Mão Diná', celular: '99236-4764' },
+//   { key: 6, nome: 'Mão Diná', celular: '99236-4764' },
+//   { key: 7, nome: 'Mão Diná', celular: '99236-4764' },
+//   { key: 8, nome: 'Mão Diná', celular: '99236-4764' },
+//   { key: 9, nome: 'Mão Diná', celular: '99236-4764' }
+// ];
 
 export default function Cadastro() {
+  const inputRef = useRef(null);
   const [nome, setNome] = useState(null);
   const [celular, setCelular] = useState(null);
+  const [coroinhas, setCoroinhas] = useState([]);
+  const [key, setKey] = useState(null);
+
+  useEffect(() => {
+    async function getDados() {
+      setCoroinhas([]);
+
+      await firebase
+        .database()
+        .ref('coroinhas') //'coroinhas/1'
+        .once('value', (snapshot) => {
+          snapshot?.forEach((childItem) => {
+            let data = {
+              key: childItem.key,
+              nome: childItem.val().nome,
+              celular: childItem.val().celular
+            };
+
+            setCoroinhas((oldCoroinhas) => [...oldCoroinhas, data]);
+          });
+          //snapshot.val().nome
+          //snapshot.val().idade
+        });
+    }
+
+    getDados();
+  }, []);
+
+  function handleAdd() {
+    if (!nome || !celular) return;
+
+    if (key) {
+      firebase
+        .database()
+        .ref('coroinhas')
+        .child(key)
+        .update({
+          nome,
+          celular
+        })
+        .then(() => {
+          const coroinhaIndex = coroinhas.findIndex(
+            (coroinha) => coroinha.key === key
+          );
+          let coroinhasClone = coroinhas;
+          coroinhasClone[coroinhaIndex].nome = nome;
+          coroinhasClone[coroinhaIndex].celular = celular;
+
+          setCoroinhas([...coroinhasClone]);
+        });
+
+      Keyboard.dismiss();
+      setNome('');
+      setCelular('');
+      setKey(null);
+      return;
+    }
+
+    let coroinha = firebase.database().ref('coroinhas');
+    let chave = coroinha.push().key;
+
+    coroinha
+      .child(chave)
+      .set({
+        nome,
+        celular
+      })
+      .then(() => {
+        const data = {
+          key: chave,
+          nome,
+          celular
+        };
+
+        setCoroinhas((oldCoroinhas) => [...oldCoroinhas, data]);
+      });
+
+    setNome(null);
+    setCelular(null);
+    Keyboard.dismiss();
+  }
+
+  function handleDelete(key) {
+    if (!key) return;
+
+    firebase
+      .database()
+      .ref('coroinhas')
+      .child(key)
+      .remove()
+      .then(() => {
+        const newCoroinhasList = coroinhas.filter((item) => item.key !== key);
+        setCoroinhas(newCoroinhasList);
+      });
+  }
+
+  function handleEdit(data) {
+    setKey(data.key);
+    setNome(data.nome);
+    setCelular(data.celular);
+    inputRef.current.focus();
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -28,6 +141,7 @@ export default function Cadastro() {
           placeholder='Nome'
           style={styles.input}
           value={nome}
+          ref={inputRef}
           onChangeText={(text) => setNome(text)}
         />
         <TextInput
@@ -37,17 +151,28 @@ export default function Cadastro() {
           onChangeText={(text) => setCelular(text)}
         />
 
-        <TouchableOpacity style={styles.btnCadastrar} onPress={() => { }}>
-          <Text style={styles.btnText}>
-            Cadastrar
-          </Text>
+        <TouchableOpacity style={styles.btnCadastrar} onPress={handleAdd}>
+          <Text style={styles.btnText}>Cadastrar</Text>
         </TouchableOpacity>
 
         <FlatList
           style={styles.list}
-          keyExtractor={item => item.key}
+          keyExtractor={(item) => item.key}
           data={coroinhas}
-          renderItem={({ item }) => (<ItemListaCoroinha data={item} />)}
+          renderItem={({ item }) => (
+            <ItemListaCoroinha
+              data={item}
+              deleteItem={handleDelete}
+              editItem={handleEdit}
+            />
+          )}
+          ListEmptyComponent={
+            <View style={{ alignItems: 'center', marginTop: 20 }}>
+              <Text style={{ fontSize: 14, color: '#ff6348' }}>
+                Nenhum coroinha cadastrado!
+              </Text>
+            </View>
+          }
         />
       </View>
     </SafeAreaView>
@@ -59,12 +184,12 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 35,
     paddingHorizontal: 10,
-    backgroundColor: '#F2f6fc',
+    backgroundColor: '#F2f6fc'
   },
   boxAreaCadastro: {
     flex: 1,
     alignItems: 'center',
-    padding: 30
+    padding: 15
   },
   titleText: {
     fontSize: 16,
@@ -91,12 +216,15 @@ const styles = StyleSheet.create({
     height: 45,
     marginBotton: 10,
     marginTop: 5,
+    borderRadius: 8,
     backgroundColor: '#0984e3',
-    width: '100%',
+    width: '100%'
   },
   list: {
     width: '100%',
-    marginTop: 15,
-    backgroundColor: '#dfe4ea',
+    marginTop: 10,
+    padding: 10,
+    borderRadius: 8,
+    backgroundColor: '#dfe4ea'
   }
 });
